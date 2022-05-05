76156b4c6d9fd0c9ed3c75dca799acaf0813034c
Abdul Ibrahim <theoneadisa@gmail.com>
2022-05-05 15:16:34 +0100

    config

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -2,9 +2,17 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV;
+const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../../config/config.js')[env];
 const db = {};
+
+// let sequelize;
+// if (config.use_env_variable) {
+// sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+// const sequelize =
+// new Sequelize(config.database, config.username, config.password, config);
+// }
 
 let sequelize;
 if (config.use_env_variable) {
