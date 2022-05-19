c64d1dec24b96f6c77079947b070865cb1104785
Abdul Ibrahim <theoneadisa@gmail.com>
2022-05-19 14:17:26 +0100

    enable cors access from remote url

--- src/app.js
+++ src/app.js
@@ -14,7 +14,17 @@ const app = express();
 const isProduction = process.env.NODE_ENV === 'production';
 
 app.use(morgan('dev'));
-app.use(cors());
+app.use(cors(
+    {
+      // origin: '*',
+      origin: ['https://www.gaged.io', 'http://localhost:3000/'],
+      methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+    },
+));
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
