2833b0c9cb3f7d496b556c0a67f0204b7091563a
Abdul Ibrahim <theoneadisa@gmail.com>
2022-05-20 10:42:07 +0100

    cors

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cookieParser from 'cookie-parser';
 import fileUpload from 'express-fileupload';
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
-import cors from 'cors';
+// import cors from 'cors';
 import morgan from 'morgan';
 import errorhandler from 'errorhandler';
 
@@ -14,17 +14,26 @@ const app = express();
 const isProduction = process.env.NODE_ENV === 'production';
 
 app.use(morgan('dev'));
-app.use(cors(
-    {
-      origin: '*',
-      // origin: ['https://www.gaged.io', 'http://localhost:3000/'],
-      methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
-    },
-));
+// app.use(cors(
+//     {
+//       origin: '*',
+//       origin: ['https://www.gaged.io', 'http://localhost:3000/'],
+//       methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+//     },
+// ));
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', '*');
 //   next();
 // });
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://www.gaged.io');
+  // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
